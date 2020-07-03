b7ee38e4560c12bdc992a1ecbbed840f044ddb41
galibce003 <66032815+galibce003@users.noreply.github.com>
2020-07-03 13:57:18 +0600

    Add files via upload

---
+++ function_using_LAMBDA_4.py
@@ -0,0 +1,12 @@
+#norml function
+def info(x):
+    return x-1
+print(info(1))
+
+
+
+#Same as before but using LAmbda
+info = lambda x : x-1   #info = function name
+                        #x = argument
+                        #x-1 = what we want to return
+print(info(1))
